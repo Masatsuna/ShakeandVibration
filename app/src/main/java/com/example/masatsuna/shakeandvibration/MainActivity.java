3ac270ca805315791e4bec140591a1f441ebeb0d
山崎優綱 <masatsuna@yamazakiyuutsunanoMacBook-Air.local>
2017-07-06 00:14:32 +0900

    Initial commit

---
+++ app/src/main/java/com/example/masatsuna/shakeandvibration/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.masatsuna.shakeandvibration;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    Vibrator vibrator;
+    Sensor sensor;
+    SensorManager sensorManager;
+    float before_y = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER) {
+            return;
+        }
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float y = event.values[1];
+            if(before_y > 0 && (before_y - y) > 3){
+                vibrator.vibrate(500);
+            }
+            before_y = y;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onPause () {
+        super.onPause();
+        // センサーへのイベントリスナーの解除
+        sensorManager.unregisterListener(this);
+    }
+}
