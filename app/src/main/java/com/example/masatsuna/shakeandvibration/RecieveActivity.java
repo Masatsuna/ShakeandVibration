ef2da3d8667ac64eb8f3afdd96eb27facac85d68
Masatsuna <bluemoon7345@gmail.com>
2017-07-09 17:37:07 +0900

    ブロードキャストレシーバ登録

--- app/src/main/java/com/example/masatsuna/shakeandvibration/RecieveActivity.java
+++ app/src/main/java/com/example/masatsuna/shakeandvibration/RecieveActivity.java
@@ -1,15 +1,23 @@
 package com.example.masatsuna.shakeandvibration;
 
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class RecieveActivity extends AppCompatActivity {
 
+    IntentFilter intentFilter;
+    MyBroadcastReceiver myBroadcastReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recieve);
+        myBroadcastReceiver = new MyBroadcastReceiver();
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("SHAKE");
+        registerReceiver(myBroadcastReceiver, intentFilter);
     }
 
     public void onClick(View view) {
